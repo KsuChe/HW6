d3ca87690be426a1aa8344634188a1c88074b3d2
KsuChe <nerro@mail.ru>
2022-05-04 22:30:31 +0300

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.stream.IntStream;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //task1
+        int[] money = generateRandomArray();
+        int sum = 0;
+        for (int element: generateRandomArray()) {
+            sum += element;
+        }
+        System.out.println(sum);
     }
-}
+
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+
+    }
+
+
+
